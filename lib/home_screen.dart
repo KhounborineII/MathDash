ecd83a39a1dd38636938174e89c8556765cf5287
Isaac Khounborine <KhounborineII@hendrix.edu>
2022-10-05 13:59:19 -0500

    Request and Request Screens added + Page Navigation
    Temporary Buttons have been added to account for the current missing network functionality. Minor UI adjustments made, building up to the desire visual appearance of the final product.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:math_dash/game_screen.dart';
+import 'package:math_dash/request_screen.dart';
+import 'package:math_dash/respond_screen.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -22,6 +24,24 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
 
+    void openRequest() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const RequestPage(),
+        ),
+      );
+    }
+
+    void openRespond() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const RespondPage(),
+        ),
+      );
+    }
+
     return WillPopScope(
       onWillPop: () async {
         return false;
@@ -35,22 +55,40 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextField(
-                onChanged: (value) {
-                  setState(() {});
-                },
-                decoration: const InputDecoration(hintText: "IP Address"),
-              ),
-              const SizedBox(height: 10),
               SizedBox(
-                  width: 300,
-                  height: 100,
+                width: 300,
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                  decoration: const InputDecoration(hintText: "IP Address"),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                  width: 250,
+                  height: 75,
                   child: ElevatedButton(
-                      key: const Key("PlayButton"),
-                      onPressed: openGame,
+                      key: const Key("InviteButton"),
+                      onPressed: openRequest,
                       style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(fontSize: 30)),
-                      child: const Text("Play!"))),
+                          textStyle: const TextStyle(fontSize: 40)),
+                      child: const Text("Invite"))),
+              const SizedBox(height: 10),
+
+              // Button Below will not exist.
+              // Button should be replaced with logic code that
+              // relates to the network when a play request is sent.
+
+              SizedBox(
+                  width: 200,
+                  height: 75,
+                  child: ElevatedButton(
+                      key: const Key("TempRespondButton"),
+                      onPressed: openRespond,
+                      style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 15)),
+                      child: const Text("Respond Dev Button"))),
             ],
           ),
         ),
---
+++ lib/respond_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:math_dash/home_screen.dart';
+import 'package:math_dash/game_screen.dart';
+
+class RespondPage extends StatefulWidget {
+  const RespondPage({super.key});
+
+  @override
+  State<RespondPage> createState() => _RespondPageState();
+}
+
+class _RespondPageState extends State<RespondPage> {
+  @override
+  Widget build(BuildContext context) {
+    void openGame() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const GamePage(),
+        ),
+      );
+    }
+
+    void openHome() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MyHomePage(title: "Math Dash!"),
+        ),
+      );
+    }
+
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("An Opponent Appears!"),
+          automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Opponent From IP: INSERT IP HERE Is Challenging You!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.red,
+                    fontFamily: 'Open Sans',
+                    fontWeight: FontWeight.w900,
+                    fontSize: 20),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                  width: 250,
+                  height: 50,
+                  child: ElevatedButton(
+                      key: const Key("PlayButton"),
+                      onPressed: openGame,
+                      style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 30)),
+                      child: const Text("Play"))),
+              const SizedBox(height: 10),
+              SizedBox(
+                  width: 250,
+                  height: 50,
+                  child: ElevatedButton(
+                      key: const Key("DenyButton"),
+                      onPressed: openHome,
+                      style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 30)),
+                      child: const Text("Deny"))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
