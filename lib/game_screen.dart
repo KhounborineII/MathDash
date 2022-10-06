4a44a65eb1d0876a937cc1436e6e87b5a6a37684
Simon <reidst@hendrix.edu>
2022-10-06 01:20:04 -0500

    Fixed navigation from Game to Results
    Navigation from Game to Results works properly; Removed "swipe to pop" functionality from Game page; Reduced timer amount from 30 seconds to 10 for easier testing

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -17,7 +17,6 @@ class _GamePageState extends State<GamePage> {
   // TODO: initialize these values in a constructor or initState call
   int thisPlayerScore = 0;
   int otherPlayerScore = 0;
-  bool isGameOver = false;
 
   late int timeLeft;
   late String currentQuestion;
@@ -30,9 +29,15 @@ class _GamePageState extends State<GamePage> {
   @override
   void initState() {
     super.initState();
-    timeLeft = 30;
+    timeLeft = 10;
     timer = Timer.periodic(const Duration(seconds: 1), countdown);
     nextQuestion();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel();
   }
 
   void nextQuestion() {
@@ -60,7 +65,12 @@ class _GamePageState extends State<GamePage> {
     } else {
       // an end message would be sent here
       setState(() {
-        isGameOver = true;
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const GameOverPage(),
+          ),
+        );
       });
       t.cancel();
     }
@@ -72,128 +82,122 @@ class _GamePageState extends State<GamePage> {
     Color defaultBlue = Theme.of(context).primaryColor;
     Color darkBlue = Theme.of(context).primaryColorDark;
 
-    if (isGameOver) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const GameOverPage(),
+    return WillPopScope(
+      onWillPop:() async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Math Dash!'),
+          automaticallyImplyLeading: false,
+          foregroundColor: Colors.white,
         ),
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Math Dash!'),
-        automaticallyImplyLeading: false,
-        foregroundColor: Colors.white,
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Row( // Scoreboard/timer at top of screen
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column( // This player's score
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'You',
-                        style: TextStyle(
-                          color: defaultBlue,
-                          fontSize: 40,
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Row( // Scoreboard/timer at top of screen
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column( // This player's score
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'You',
+                          style: TextStyle(
+                            color: defaultBlue,
+                            fontSize: 40,
+                          ),
                         ),
-                      ),
-                      Text(
-                        '$thisPlayerScore',
-                        style: TextStyle(
-                          color: defaultBlue,
-                          fontSize: 40,
+                        Text(
+                          '$thisPlayerScore',
+                          style: TextStyle(
+                            color: defaultBlue,
+                            fontSize: 40,
+                          ),
+                          key: const Key('thisPlayerScoreText'),
                         ),
-                        key: const Key('thisPlayerScoreText'),
-                      ),
-                    ],
-                  ),
-                  Column( // Timer
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.timer_outlined, 
-                        color: defaultBlue,
-                        size: 50,
-                      ),
-                      Text(
-                        '$timeLeft',
-                        style: TextStyle(
+                      ],
+                    ),
+                    Column( // Timer
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.timer_outlined, 
                           color: defaultBlue,
-                          fontSize: 40,
+                          size: 50,
                         ),
-                        key: const Key('timeLeftText'),
-                      ),
-                    ],
-                  ),
-                  Column( // Other player's score
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Them',
-                        style: TextStyle(
-                          color: defaultBlue,
-                          fontSize: 40,
+                        Text(
+                          '$timeLeft',
+                          style: TextStyle(
+                            color: defaultBlue,
+                            fontSize: 40,
+                          ),
+                          key: const Key('timeLeftText'),
                         ),
-                      ),
-                      Text(
-                        '$otherPlayerScore',
-                        style: TextStyle(
-                          color: defaultBlue,
-                          fontSize: 40,
+                      ],
+                    ),
+                    Column( // Other player's score
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Them',
+                          style: TextStyle(
+                            color: defaultBlue,
+                            fontSize: 40,
+                          ),
                         ),
-                        key: const Key('otherPlayerScoreText'),
-                      ),
-                    ],
-                  ),
-                ],
+                        Text(
+                          '$otherPlayerScore',
+                          style: TextStyle(
+                            color: defaultBlue,
+                            fontSize: 40,
+                          ),
+                          key: const Key('otherPlayerScoreText'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 130),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text( // Current math question
-                    currentQuestion,
-                    style: TextStyle(
-                          color: darkBlue,
-                          fontSize: 60,
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 130),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text( // Current math question
+                      currentQuestion,
+                      style: TextStyle(
+                            color: darkBlue,
+                            fontSize: 60,
+                            fontWeight: FontWeight.bold,
+                          ),
+                      key: const Key('CurrentQuestionText'),
+                    ),
+                    SizedBox(
+                      width: 100,
+                      child: TextField( // Input field
+                        key: const Key('answerTextField'),
+                        autofocus: true,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.orange,
+                          fontSize: 40,
                           fontWeight: FontWeight.bold,
                         ),
-                    key: const Key('CurrentQuestionText'),
-                  ),
-                  SizedBox(
-                    width: 100,
-                    child: TextField(
-                      key: const Key('answerTextField'),
-                      autofocus: true,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
+                        controller: answerInputController,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                        textInputAction: TextInputAction.next,
+                        onSubmitted: checkAnswer,
                       ),
-                      style: const TextStyle(
-                        color: Colors.orange,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      controller: answerInputController,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      textInputAction: TextInputAction.next,
-                      onSubmitted: checkAnswer,
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
