ca021ce3424c5fdbaa46ef99bde9cd6461aa9a4a
Josh <thomeczekje@hendrix.edu>
2022-10-07 01:08:26 -0500

    Early set-up for peer-to-peer

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:math_dash/game_screen.dart';
 import 'package:math_dash/request_screen.dart';
 import 'package:math_dash/respond_screen.dart';
+import 'package:network_info_plus/network_info_plus.dart';
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -13,6 +17,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //
+
+  // why is this important? I'm not sure!
+  int ourPort = 8888;
+  //
+
+  //
+
+  String? _ipAddress = "";
+  late StreamSubscription<Socket> server_sub;
+
+  void initState() {
+    super.initState();
+    //_nameController = TextEditingController();
+    //_ipController = TextEditingController();
+    _setupServer();
+    _findIPAddress();
+  }
+
+  void dispose() {
+    server_sub.cancel();
+    super.dispose();
+  }
+
+  Future<void> _findIPAddress() async {
+    // Thank you https://stackoverflow.com/questions/52411168/how-to-get-device-ip-in-dart-flutter
+    String? ip = await NetworkInfo().getWifiIP();
+    setState(() {
+      _ipAddress = ip;
+    });
+  }
+
+  Future<void> _setupServer() async {
+    try {
+      ServerSocket server =
+          await ServerSocket.bind(InternetAddress.anyIPv4, ourPort);
+      server_sub = server.listen(_listenToSocket); // StreamSubscription<Socket>
+    } on SocketException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Error: $e"),
+      ));
+    }
+  }
+
+  void _listenToSocket(Socket socket) {
+    socket.listen((data) {
+      setState(() {
+        _handleIncomingMessage(socket.remoteAddress.address, data);
+      });
+    });
+  }
+
+  void _handleIncomingMessage(String ip, Uint8List incomingData) {
+    String received = String.fromCharCodes(incomingData);
+    print("Received '$received' from '$ip'");
+    //
+    // HERE'S WHERE ONE PHONE WOULD RECEIVE A CHALLENGE I THINK
+    //
+  }
+
+  //
+
+  //
+
+  //
+
   @override
   Widget build(BuildContext context) {
     void openGame() {
@@ -24,7 +94,25 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
 
+    Future<void> sendRequest() async {
+      Socket socket = await Socket.connect(OPPONENTipAddress, ourPort);
+      socket.write("request");
+      socket.close();
+      //From messenger app:
+      //await _add_message("Me", message);
+
+      //Future<void> _add_message(String name, String message) async {
+      //  await m.protect(() async {
+      //    _messages.add(Message(author: name, content: message));
+      //    notifyListeners();
+      //  });
+      //}
+    }
+
     void openRequest() {
+      //send request to opponent's ip
+      sendRequest;
+
       Navigator.push(
         context,
         MaterialPageRoute(
