277ca39085f9c8cacd8e859b885e83228fbb5ad9
Simon <reidst@hendrix.edu>
2022-10-09 00:12:25 -0500

    Created a class for network messages
    Created Message class and MessageType enum to structure message data and handle conversion to/from JSON; created unit tests for Messages

--- lib/communication.dart
+++ lib/communication.dart
@@ -1,20 +1,28 @@
+enum MessageType {
+  invite,
+  ignore,
+  rsvp,
+  update,
+  end;
+}
+
 class Message {
 
   Message(this._version, this._type, this._value);
   late final int _version;
-  late final String _type;
-  late final dynamic _value;
+  late final MessageType _type;
+  late final Map<String, dynamic> _value;
 
   int get version => _version;
-  String get type => _type;
-  dynamic get value => _value;
+  MessageType get type => _type;
+  Map<String, dynamic> get value => _value;
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> valueToReturn = 
-      _type == 'invite' ? {'host': _value} :
-      _type == 'rsvp'   ? {'response': _value == -1, 'seed': _value} :
-      _type == 'update' ? {'new_score': _value} :
-      _type == 'end'    ? {'final_score': _value} :
+      _type == MessageType.invite ? {'host': _value} :
+      _type == MessageType.rsvp   ? {'response': _value == -1, 'seed': _value} :
+      _type == MessageType.update ? {'new_score': _value} :
+      _type == MessageType.end    ? {'final_score': _value} :
       {};
     return {'version': _version, 'type': _type, 'value': valueToReturn};
   }
@@ -23,9 +31,9 @@ class Message {
     : _version = json['version'],
       _type = json['type'],
       _value = 
-        json['type'] == 'invite' ? json['value']['host'] :
-        json['type'] == 'rsvp'   ? json['value']['seed'] :
-        json['type'] == 'update' ? json['value']['new_score'] :
-        json['type'] == 'end'    ? json['value']['final_score']:
+        json['type'] == MessageType.invite ? json['value']['host'] :
+        json['type'] == MessageType.rsvp   ? json['value']['seed'] :
+        json['type'] == MessageType.update ? json['value']['new_score'] :
+        json['type'] == MessageType.end    ? json['value']['final_score']:
         null;
 }
